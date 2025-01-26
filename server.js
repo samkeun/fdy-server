117b0dad4a03e41bf62a40c80881c97299215921
samkeun <skimok@gmail.com>
2025-01-26 11:44:00 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
